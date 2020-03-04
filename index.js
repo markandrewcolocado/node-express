d5f7b5ae806e6cc17765aad3924d7764d0681811
Mark Andrew Colocado <mark05@simplexi.com>
2020-03-04 10:31:02 +0800

    modified index

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
 // Bring in express
 const express = require('express');
-const path = require('path');
-const exphbs = require('express-handlebars');
+// const path = require('path');
+// const exphbs = require('express-handlebars');
 // const logger = require('./middleware/logger');
-const apiRouter = require('./routes/api/members');
-const members = require('./Members');
+// const apiRouter = require('./routes/api/members');
+// const members = require('./Members');
 
 // Initialize express
 const app = express();
 
 // Handlebars middleware
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-
-// Body parser middleware
-app.use(express.json());
-
-// Handle form submissions
-app.use(express.urlencoded({ extended: false}));
-
-app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Member App',
-        members
-    });
-});
+// app.engine('handlebars', exphbs());
+// app.set('view engine', 'handlebars');
+//
+// // Body parser middleware
+// app.use(express.json());
+//
+// // Handle form submissions
+// app.use(express.urlencoded({ extended: false}));
+//
+// app.get('/', (req, res) => {
+//     res.render('index', {
+//         title: 'Member App',
+//         members
+//     });
+// });
 
 // Initialize logger middleware
 // app.use(logger);
 
 // Create a route
-// app.get('/', (req, res) => {
-//     // res.send('<h1>Hello World!!!</h1>');
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!!!</h1>');
 //     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
+});
 
-// Set a static folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Members API Routes
-app.use('/api/members', apiRouter);
+// // Set a static folder
+// app.use(express.static(path.join(__dirname, 'public')));
+//
+// // Members API Routes
+// app.use('/api/members', apiRouter);
 
 // Define the port number
 const PORT = process.env.PORT || 5000;
